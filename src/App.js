4449683dfe3672d41586324d2e7389f1215a10e7
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-10-21 15:54:03 +0200

    Added navigation component for easy navigation

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return <div>
+    <MainNavigation />
     <Switch>
       <Route path='/' exact>
         <AllMeetupsPage />
