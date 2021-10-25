5961fdff8fb0d3ad308a37bc494c8577fd28a1eb
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-10-25 16:20:54 +0200

    Begynt på add new meetup page

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,12 @@
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
+
+function NewMeetupForm() {
+  return <Card>
+    <form class>
+
+    </form>
+  </Card>
+}
+
+export default NewMeetupForm;
