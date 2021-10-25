d308d91ca92ad43359f9e5a1b94d66de75217db3
Mohammad Tayyab Khalid <tayyabkhalid306@gmail.com>
2021-10-25 15:32:35 +0200

    Nettsiden er riktig strukturert

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -3,12 +3,12 @@ import classes from './MeetupItem.module.css';
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
-      <div classNAME={classes.image}>
-        <img src={props.img} alt={props.title} />
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
       </div>
-      <div classNAME={classes.content}>
-        <h3>{props.title[</h3>
-        <address>p{rops.address]</address>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
         <p>{props.description}</p>
       </div>
       <div className={classes.actions}>
